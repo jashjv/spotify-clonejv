a2c0445a5701945b452ddafa5cd916993cab09bd
jashwanth <jashwanth@sameersolutions.com>
2022-12-13 11:15:56 +0530

    auth

--- src/Spotify.js
+++ src/Spotify.js
@@ -10,7 +10,7 @@ const scopes = [
     "user-read-currently-playing",
     "user-read-recently-played",
     "user-read-playback-state",
-    "user-top-read",
+    "user-top-read", 
     "user-modify-playback-state"
 ];
 
